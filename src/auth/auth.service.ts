a21032b14451a4cb6ab575f47dd0ff5987af1caa
Armando Ribamar <armando.ribamar@gmail.com>
2025-08-13 17:02:51 -0300

    Implement user and post modules with CRUD operations; add DTOs and validation

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,8 +1,12 @@
 import { Injectable } from '@nestjs/common';
+import { LoginDto } from './dto/login.dto';
 
 @Injectable()
 export class AuthService {
-  login() {
-    return 'Olá do AuthService';
+  login(loginDto: LoginDto) {
+    /* console.log(loginDto); */
+    /* return 'Olá do AuthService'; */
+    console.log(loginDto.email, loginDto.password);
+    return loginDto;
   }
 }
---
+++ src/user/user.controller.ts
@@ -0,0 +1,9 @@
+import { Controller, Get, Param } from '@nestjs/common';
+
+@Controller('user')
+export class UserController {
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return `Olá return do user #${id}`;
+  }
+}
