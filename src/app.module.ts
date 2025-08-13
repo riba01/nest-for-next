40be06afa6e029408e1c70145768c9443ec74763
Armando Ribamar <armando.ribamar@gmail.com>
2025-08-13 19:50:49 -0300

    feat: integrate TypeORM with better-sqlite3 and PostgreSQL support
    - Added @nestjs/typeorm and better-sqlite3 dependencies to package.json.
    - Configured TypeOrmModule in app.module.ts to support dynamic database configuration based on environment variables.
    - Updated UserController to utilize ConfigService for environment variable access.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,49 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
-import { UserModule } from './user/user.module';
 import { PostModule } from './post/post.module';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [AuthModule, UserModule, PostModule],
+  imports: [
+    AuthModule,
+    UserModule,
+    PostModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    /*  TypeOrmModule.forRoot({
+      type: 'better-sqlite3',
+      database: './db.sqlite',
+      synchronize: true,
+      autoLoadEntities: true,
+    }), */
+    TypeOrmModule.forRootAsync({
+      useFactory: () => {
+        if (process.env.DB_TYPE === 'better-sqlite3') {
+          return {
+            type: process.env.DB_TYPE,
+            database: process.env.DB_DATABASE || './db.sqlite',
+            synchronize: process.env.DB_SYNCHRONIZE === '1',
+            autoLoadEntities: process.env.DB_AUTOLOAD_ENTITIES === '1',
+          };
+        }
+        return {
+          type: 'postgres',
+          host: process.env.DB_HOST,
+          port: parseInt(process.env.DB_PORT || '5432', 10),
+          username: process.env.DB_USERNAME,
+          password: process.env.DB_PASSWORD,
+          database: process.env.DB_DATABASE,
+          synchronize: process.env.DB_SYNCHRONIZE === '1',
+          autoLoadEntities: process.env.DB_AUTOLOAD_ENTITIES === '1',
+        };
+      },
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
   exports: [],
--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,11 +1,16 @@
 import { Controller, Get, Param } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { CustomParseIntPipe } from '../common/pipes/custom-parse-int-pipe';
 
 @Controller('user')
 export class UserController {
+  constructor(private readonly configService: ConfigService) {}
+
   @Get(':id')
   findOne(@Param('id', CustomParseIntPipe) id: number) {
     //console.log(id, typeof id);
+    /* console.log(process.env.TESTE); */
+    console.log(this.configService.get('TESTE1', 'Valor padrão'));
     return `Olá return do user #${id}`;
   }
 }
